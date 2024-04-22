cb67680c1b850730fbe774d7ccf235efa8f49c28
Nishigandha-Kasar <143502853+Nishigandha-Kasar@users.noreply.github.com>
2024-04-22 17:52:45 +0530

    Update authReducer.js

--- src/authReducer.js
+++ src/authReducer.js
@@ -1,17 +1,30 @@
-// authReducer.js
 const initialState = {
-    isAuthenticated: false,
-    user: null,
-    // Add other authentication-related state variables
-  };
-  
-  const authReducer = (state = initialState, action) => {
-    switch (action.type) {
-      // Handle authentication-related actions here
-      default:
-        return state;
-    }
-  };
-  
-  export default authReducer;
-  
+  isAuthenticated: true,
+  user: {
+    "id": 1,
+    "name": "admin",    
+    "password": "123"
+  },
+  loading: false,
+  error: null,
+};
+
+const authReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case 'LOGIN_SUCCESS':
+      return {
+        ...state,
+        isAuthenticated: true,
+        user: action.payload, // Update user state with received data
+      };
+    case 'LOGOUT':
+      return {
+        isAuthenticated: false,
+        user: null,
+      };
+    default:
+      return state;
+  }
+};
+
+export default authReducer;
